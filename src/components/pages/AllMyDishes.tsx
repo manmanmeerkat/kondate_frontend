981a6914e18dadaa5280f4cb06a02f97bb192b87
manmanmeer <masak6161@gmail.com>
2023-10-14 15:42:37 +0900

    search一部ひとまず完成

--- src/components/pages/AllMyDishes.tsx
+++ src/components/pages/AllMyDishes.tsx
@@ -1,7 +1,16 @@
 // AllMyDishes.tsx
 import React, { memo, useCallback, useEffect, useState } from "react";
-import { Header } from "../organisms/layout/Header";
-import { Center, Spinner, Wrap, WrapItem, useDisclosure, Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
+import {
+  Center,
+  Spinner,
+  Wrap,
+  WrapItem,
+  useDisclosure,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from "@chakra-ui/react";
 import { useDishData } from "../../hooks/useDishData";
 import { useAllMyDishes } from "../../hooks/useAllMyDishes";
 import { DishCard } from "../organisms/dishes/DishCard";
@@ -10,6 +19,8 @@ import { DishDetailModal } from "../organisms/dishes/DisheDetailModal";
 import { GenreButton } from "../molecules/GenreButton";
 import { Dish } from "../../types/Dish";
 import { SearchIcon } from "@chakra-ui/icons";
+import { useIngredientSearch } from "../../hooks/useIngredientSearch";
+import { Header } from "../organisms/layout/Header";
 
 interface AllMyDishesProps {}
 
@@ -18,6 +29,7 @@ export const AllMyDishes: React.FC<AllMyDishesProps> = memo(() => {
   const { getDishes, dishes, loading } = useAllMyDishes();
   const { onSelectDish, selectedDish } = useSelectDish();
   const { getDish, dishData } = useDishData();
+  const { searchedRecipes, handleIngredientSearch } = useIngredientSearch();
 
   useEffect(() => {
     getDish();
@@ -25,96 +37,92 @@ export const AllMyDishes: React.FC<AllMyDishesProps> = memo(() => {
 
   const [selectedDishId, setSelectedDishId] = useState<number | null>(null);
   const [searchIngredient, setSearchIngredient] = useState<string>("");
-  const [searchedRecipes, setSearchedRecipes] = useState<Dish[]>([]);
+  const [noSearchResults, setNoSearchResults] = useState<boolean>(false);
 
-  const onClickDish = useCallback((id: number) => {
-    onSelectDish({ id, dishes, onOpen });
-    setSelectedDishId(id);
-  }, [dishes, onSelectDish, onOpen]);
+  const onClickDish = useCallback(
+    (id: number) => {
+      onSelectDish({ id, dishes, onOpen });
+      setSelectedDishId(id);
+    },
+    [dishes, onSelectDish, onOpen]
+  );
 
-  const handleIngredientSearch = async () => {
-    try {
-      if (searchIngredient.trim() === "") {
-        // 検索条件が空の場合、すべてのレシピを表示
-        setSearchedRecipes([]);
-      } else {
-        const response = await fetch(`http://localhost:8000/api/recipes/search?ingredient=${searchIngredient}`);
-        const data = await response.json();
-  
-        if (response.ok) {
-          setSearchedRecipes(data.recipes);
-        } else {
-          console.error("検索に失敗しました。");
-        }
-      }
-    } catch (error) {
-      console.error("通信エラー:", error);
-    }
-  };
+  const handleSearchButtonClick = useCallback(async () => {
+    const results = await handleIngredientSearch(searchIngredient);
+    setNoSearchResults(results.length === 0 && searchIngredient.trim() !== "");
+  }, [handleIngredientSearch, searchIngredient]);
 
-
-return (
-  <div>
-    <Header />
-    <GenreButton />
-    <InputGroup mt={4} mx="auto" w={{ base: "80%", md: "60%" }}>
-      <Input
-        placeholder="材料で検索"
-        value={searchIngredient}
-        onChange={(e) => setSearchIngredient(e.target.value)}
+  return (
+    <div>
+      <Header />
+      <GenreButton />
+      <InputGroup mt={4} mx="auto" w={{ base: "80%", md: "60%" }}>
+        <Input
+          placeholder="材料で検索"
+          value={searchIngredient}
+          onChange={(e) => setSearchIngredient(e.target.value)}
+        />
+        <InputRightElement width="4.5rem">
+          {/* 検索ボタン */}
+          <Button colorScheme="teal" onClick={handleSearchButtonClick} size="sm">
+            <SearchIcon />
+            検索
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      {loading ? (
+        <Center h="50vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {noSearchResults ? (
+            <Center h="50vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+              <p>該当するデータがありません。</p>
+            </Center>
+          ) : (
+            <>
+              {searchedRecipes.length > 0 ? (
+                searchedRecipes.map((dish) => (
+                  <WrapItem key={dish.id} mx="auto">
+                    <DishCard
+                      id={dish.id}
+                      imageUrl={dish.image_path}
+                      menuType="Japanese"
+                      dishName={dish.name}
+                      onClick={onClickDish}
+                    />
+                  </WrapItem>
+                ))
+              ) : (
+                dishData && dishData.recipes && dishData.recipes.length > 0 ? (
+                  dishData.recipes.map((dish: any) => (
+                    <WrapItem key={dish.id} mx="auto">
+                      <DishCard
+                        id={dish.id}
+                        imageUrl={dish.image_path}
+                        menuType="Japanese"
+                        dishName={dish.name}
+                        onClick={onClickDish}
+                      />
+                    </WrapItem>
+                  ))
+                ) : (
+                  <Center h="50vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                    <Spinner />
+                  </Center>
+                )
+              )}
+            </>
+          )}
+        </Wrap>
+      )}
+      <DishDetailModal
+        dish={selectedDish as { id: number; name: string; genre: string; reference_url: string } | null}
+        isOpen={isOpen}
+        onClose={onClose}
+        id={selectedDishId}
       />
-      <InputRightElement width="4.5rem">
-        {/* 検索ボタン */}
-        <Button colorScheme="teal" onClick={handleIngredientSearch} size="sm">
-          <SearchIcon />
-          検索
-        </Button>
-      </InputRightElement>
-    </InputGroup>
-    {loading ? (
-      <Center h="50vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-        <Spinner />
-      </Center>
-    ) : (
-      <Wrap p={{ base: 4, md: 10 }} justify="center">
-        {searchedRecipes.length > 0 ? (
-          searchedRecipes.map((dish) => (
-            <WrapItem key={dish.id} mx="auto">
-              <DishCard
-                id={dish.id}
-                imageUrl={dish.image_path}
-                menuType="Japanese"
-                dishName={dish.name}
-                onClick={onClickDish}
-              />
-            </WrapItem>
-          ))
-        ) : dishData && dishData.recipes && dishData.recipes.length > 0 ? (
-          dishData.recipes.map((dish: any) => (
-            <WrapItem key={dish.id} mx="auto">
-              <DishCard
-                id={dish.id}
-                imageUrl={dish.image_path}
-                menuType="Japanese"
-                dishName={dish.name}
-                onClick={onClickDish}
-              />
-            </WrapItem>
-          ))
-        ) : (
-          <Center h="50vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-            <Spinner />
-          </Center>
-        )}
-      </Wrap>
-    )}
-    <DishDetailModal
-      dish={selectedDish as { id: number; name: string; genre: string; reference_url: string } | null}
-      isOpen={isOpen}
-      onClose={onClose}
-      id={selectedDishId}
-    />
-  </div>
-);
-
+    </div>
+  );
 });
---
+++ src/hooks/useIngredientSearch.tsx
@@ -0,0 +1,33 @@
+// useIngredientSearch.js
+import { useState } from 'react';
+import { Dish } from '../types/Dish';
+
+export const useIngredientSearch = () => {
+  const [searchedRecipes, setSearchedRecipes] = useState<Dish[]>([]);
+
+  const handleIngredientSearch = async (searchIngredient: string): Promise<Dish[]> => {
+    try {
+      if (searchIngredient.trim() === "") {
+        // 検索条件が空の場合、すべてのレシピを表示
+        setSearchedRecipes([]);
+        return [];
+      } else {
+        const response = await fetch(`http://localhost:8000/api/recipes/search?ingredient=${searchIngredient}`);
+        const data = await response.json();
+  
+        if (response.ok) {
+          setSearchedRecipes(data.recipes);
+          return data.recipes; // dataが直接配列の場合
+        } else {
+          console.error("検索に失敗しました。");
+          return [];
+        }
+      }
+    } catch (error) {
+      console.error("通信エラー:", error);
+      return [];
+    }
+  };
+
+  return { searchedRecipes, handleIngredientSearch };
+};
