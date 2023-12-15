e2fde5188e4089dcab690235ed0f067714669ad7
manmanmeer <masak6161@gmail.com>
2023-12-15 22:01:40 +0900

    材料リスト　仮作成

--- src/components/pages/IngredientLists.tsx
+++ src/components/pages/IngredientLists.tsx
@@ -3,25 +3,25 @@ import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import { ja } from 'date-fns/locale';
 import { registerLocale } from 'react-datepicker';
-import { Stack, Button, Input, InputGroup, InputLeftAddon, Box, Heading, List, ListItem, Text } from '@chakra-ui/react';
-import { Header } from '../organisms/layout/Header';
+import { Stack, Button, InputGroup, Box, Heading, List, ListItem, Text, Menu } from '@chakra-ui/react';
 
 interface Menu {
-  id: number;
+  menu_id: number;
   date: string;
-  dish: {
-    name: string;
-  };
-}
-
-interface Ingredient {
-  id: number;
-  name: string;
+  dish_name: string;
+  ingredients: string[];
 }
 
 interface MenuData {
+  menu_id: number;
   menus: Menu[];
-  ingredients: Ingredient[];
+  ingredients: string[];
+  date: string;
+  dish_name: string;
+}
+
+interface ResponseData {
+  menuData: MenuData[];
 }
 
 // ロケールを設定
@@ -39,43 +39,41 @@ const SearchForm: React.FC<{ onSearch: (startDate: string, endDate: string) => v
   return (
     <Stack spacing={4} mb={8} direction="row" align="center">
       <InputGroup>
-        <DatePicker 
+        <DatePicker
           dateFormat="yyyy/MM/dd"
-          selected={startDate} 
-          onChange={setStartDate} 
-          placeholderText='開始日を選択'
+          selected={startDate}
+          onChange={(date) => setStartDate(date as Date)}
+          placeholderText="開始日を選択"
           customInput={
             <Box
               as={Button}
-              // bg="white"
               _hover={{ cursor: 'pointer' }}
               _focus={{ outline: 'none' }}
               _active={{ outline: 'none' }}
               w="100%"
               textAlign="left"
             >
-            {startDate? startDate.toLocaleDateString('ja', { year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'short' }) : '開始日を選択'}
+              {startDate ? startDate.toLocaleDateString('ja', { year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'short' }) : '開始日を選択'}
             </Box>
           }
         />
-          <Text mx={4} my={2}>～</Text>
-        <DatePicker 
+        <Text mx={4} my={2}>～</Text>
+        <DatePicker
           dateFormat="yyyy/MM/dd"
-          selected={endDate} 
-          onChange={setEndDate} 
+          selected={endDate}
+          onChange={(date) => setEndDate(date as Date)}
           locale={ja}
-          placeholderText='終了日を選択'
+          placeholderText="終了日を選択"
           customInput={
             <Box
               as={Button}
-              // bg="white"
               _hover={{ cursor: 'pointer' }}
               _focus={{ outline: 'none' }}
               _active={{ outline: 'none' }}
               w="100%"
               textAlign="left"
             >
-            {endDate ? endDate.toLocaleDateString('ja', { year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'short' }) : '終了日を選択'}
+              {endDate ? endDate.toLocaleDateString('ja', { year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'short' }) : '終了日を選択'}
             </Box>
           }
         />
@@ -87,61 +85,72 @@ const SearchForm: React.FC<{ onSearch: (startDate: string, endDate: string) => v
   );
 };
 
-
 export const IngredientsList: React.FC = () => {
-  const [menuData, setMenuData] = useState<MenuData | null>(null);
+  const [menuData, setMenuData] = useState<MenuData[] | null>(null);
 
   const handleSearch = async (startDate: string, endDate: string) => {
     try {
       // バックエンドに日付範囲を送信し、結果を取得
-      const response = await axios.get<MenuData>('http://localhost:8000/api/menus', {
+      const response = await axios.get<ResponseData>('http://localhost:8000/api/get-ingredients-list', {
         params: {
           start_date: startDate,
           end_date: endDate,
         },
       });
-
-      // 取得したデータをセット
-      setMenuData(response.data);
+  
+      // 取得したデータを確認
+      
+  
+      // menuData プロパティが存在することを確認
+      if (response.data && response.data.menuData) {
+        
+        setMenuData(response.data.menuData);
+        console.log(menuData);
+      } else {
+        console.error('Menu data is not available in the response.');
+      }
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
+  
 
   return (
     <>
-    <Header />
       <Box p={8}>
         <Heading mb={4}>材料リスト</Heading>
         <SearchForm onSearch={handleSearch} />
 
-        {menuData ? (
-          <Box>
-            <Heading as="h2" size="md" mb={2}>
-              Menu
-            </Heading>
-            <List>
-              {menuData.menus.map((menu) => (
-                <ListItem key={menu.id}>
-                  {menu.date} - {menu.dish.name}
-                </ListItem>
-              ))}
-            </List>
+        {menuData && (
+  <Box>
+    <Heading as="h2" size="md" mb={2}>
+      メニュー
+    </Heading>
+    <List>
+      {menuData.map((menu) => (
+        <ListItem key={menu.menu_id}>
+          {menu.date} - {menu.dish_name}
+        </ListItem>
+      ))}
+    </List>
 
-            <Heading as="h2" size="md" mt={4} mb={2}>
-              Ingredients
-            </Heading>
-            <List>
-              {menuData.ingredients.map((ingredient) => (
-                <ListItem key={ingredient.id}>{ingredient.name}</ListItem>
-              ))}
-            </List>
-          </Box>
-        ) : (
-          <Text>No data available</Text>
-        )}
+    <Heading as="h2" size="md" mt={4} mb={2}>
+      材料
+    </Heading>
+    <List>
+      {menuData.map((menuItem) => (
+        <ListItem key={menuItem.menu_id}>
+          {menuItem.ingredients.map((ingredient, index) => (
+            <ListItem key={index}>{ingredient}</ListItem>
+          ))}
+        </ListItem>
+      ))}
+    </List>
+  </Box>
+)}
+
       </Box>
-      </>
+    </>
   );
 };
 
