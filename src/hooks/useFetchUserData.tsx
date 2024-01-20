0eb0e939e8b8e00245ed40f7bdb9adb175d4499b
manmanmeer <masak6161@gmail.com>
2024-01-20 20:13:26 +0900

    LoginPage.tsx修正

--- src/hooks/useFetchUserData.tsx
+++ src/hooks/useFetchUserData.tsx
@@ -29,6 +29,7 @@ export const useFetchUserData = (): FetchUserDataHook => {
       if (response.status === 200) {
         const userData = response.data;
         setUser(userData);
+        console.log('ユーザー情報を取得しました:', userData);
       } else {
         console.error('サーバーレスポンスエラー:', response);
       }
