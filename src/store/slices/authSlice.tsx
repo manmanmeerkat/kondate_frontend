424e8b7debe0df37af1331c986f323441190b97a
manmanmeer <masak6161@gmail.com>
2024-02-17 14:18:01 +0900

    authSlice.tsx修正

--- src/store/slices/authSlice.tsx
+++ src/store/slices/authSlice.tsx
@@ -2,6 +2,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { AuthUserType } from "../../types/AuthUserType";
 import { RootState } from "..";
+import { useEffect, useState } from "react";
+import config from "../../components/pages/config/production";
 
 // stateの初期値
 const initialState: AuthUserType = {
@@ -10,12 +12,35 @@ const initialState: AuthUserType = {
     error: undefined,
 };
 
+const [csrfToken, setCsrfToken] = useState<string>('');
+
+useEffect(() => {
+    const fetchCsrfToken = async () => {
+      try {
+        const response = await axios.get(`${config.API_ENDPOINT}/api/sanctum/csrf-cookie`, {
+          withCredentials: true,
+        });
+        const csrfToken = response.data.csrfToken;
+        setCsrfToken(csrfToken);
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
+        console.log('CSRFトークンを取得しました', csrfToken);
+      } catch (error) {
+        console.error('CSRFトークンの取得に失敗しました', error);
+      }
+    };
+
+    fetchCsrfToken(); // 非同期処理の完了を待つ
+  }, []);
+
 // ログイン中のユーザー情報を取得するAPIを叩く関数
 export const fetchAuthUser = createAsyncThunk(
     "auth/fetchAuthUser",
     async () => {
         const response = await axios.get("/api/user", {
             withCredentials: true, // クッキーを使うための設定
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+              },
             });
         return response.data;
     }
