e9544368ee4df1fe4b10b6874a2d0f2bef57925d
manmanmeer <masak6161@gmail.com>
2024-02-17 16:57:41 +0900

    App.jsxにして実験

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import * as React from "react"
-import { ChakraProvider, theme } from "@chakra-ui/react"
+import {
+  ChakraProvider,
+  theme,
+} from "@chakra-ui/react"
 import { Router } from "./components/router/Router"
 import { BrowserRouter } from "react-router-dom"
-import { Provider } from "react-redux"
+import { Provider, useDispatch } from "react-redux"
 import store from "./store"
 import axios from 'axios';
 import config from './components/pages/config/production';
 import { fetchAuthUser } from "./store/slices/authSlice"
+import { AsyncThunkAction } from "@reduxjs/toolkit"
+
 
 axios.defaults.baseURL = config.API_ENDPOINT;
 
 export const App = () => {
-  React.useEffect(() => {
-    store.dispatch(fetchAuthUser());
-  }, []);
+  const dispatch = useDispatch();
 
+  React.useEffect(() => {
+    dispatch(fetchAuthUser());
+  }, []);
+  
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
