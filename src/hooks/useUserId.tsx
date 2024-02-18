d05ecc819e8e95db69f86d92701e21439c261ccf
manmanmeer <masak6161@gmail.com>
2024-02-18 20:07:35 +0900

    useUserId.tsx修正

--- src/hooks/useUserId.tsx
+++ src/hooks/useUserId.tsx
@@ -18,6 +18,7 @@ const useUserId = () => {
             'X-CSRF-TOKEN': csrfToken,
           },
         });
+        console.log('userResponse:', userResponse);
         const fetchedUserId = userResponse.data.id;
         setUserId(fetchedUserId);
       } catch (error) {
