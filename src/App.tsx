a537703df857ac9c085bd019a8b77f14ce62baee
manmanmeer <masak6161@gmail.com>
2024-02-18 14:18:51 +0900

    App.tsx修正

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,16 @@ import store, { AppDispatch } from "./store"
 import axios from 'axios';
 import config from './components/pages/config/production';
 import { AsyncThunkAction } from "@reduxjs/toolkit"
+import { fetchAuthUser } from "./store/slices/authSlice"
 
 
 axios.defaults.baseURL = config.API_ENDPOINT;
 
 export const App = () => {
 
-  
+React.useEffect(() => {
+    store.dispatch(fetchAuthUser());
+}, []);
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
