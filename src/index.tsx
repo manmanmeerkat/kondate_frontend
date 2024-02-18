fd63b22c08f0f0cedd51d83a9f778cbddf739980
manmanmeer <masak6161@gmail.com>
2024-02-18 13:47:48 +0900

    index.tsx修正

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
+import { Provider } from "react-redux"
+import store from "./store"
 
 
 const container = document.getElementById("root")
@@ -11,10 +13,12 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container)
 
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <ColorModeScript />
     <App />
   </React.StrictMode>,
+  </Provider>
 )
 
 // If you want your app to work offline and load faster, you can change
