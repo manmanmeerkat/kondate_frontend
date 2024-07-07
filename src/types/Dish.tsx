da93438b946d34b16b320d27f60011a42af99d6e
manmanmeer <masak6161@gmail.com>
2024-07-07 21:05:45 +0900

    Dish.tsx修正

--- src/types/Dish.tsx
+++ src/types/Dish.tsx
@@ -1,5 +1,13 @@
 export interface Dish {
-    id: number;
-    name: string;
-    image_path: string;
-  }
+  id: number;
+  name: string;
+  image_path: string;
+  genre_id: number;
+  category_id: number;
+  description: string;
+  reference_url: string;
+  user_id: number;
+  created_at: string;
+  updated_at: string;
+  dishes: Dish[];
+}
