9eea205eafbbc6c1d3f10d5fede000045a844b87
manmanmeer <masak6161@gmail.com>
2024-08-16 20:37:46 +0900

    About.tsx修正

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -5,7 +5,7 @@ export const About = memo(() => {
   return (
     <Box p={4} shadow="md" borderWidth="1px" borderRadius="md">
       <Heading as="h1" size="lg" mb={4}>
-        こんだてずかんとは
+        こんだてずかん
       </Heading>
       <Text>
         このアプリは、自分が作った料理を記録して管理するためのアプリです。
@@ -25,40 +25,26 @@ export const About = memo(() => {
       <Text>
         新しく料理を登録するには、画面上の「新規登録」ボタンをクリックしてください。
       </Text>
-      <Text>
-        登録した料理は、一覧画面で確認することができます。
-      </Text>
-      <Text>
-        一覧画面の料理名をクリックすると、詳細画面に遷移します。
-      </Text>
-      <Text>
-        詳細画面では、料理の詳細を確認することができます。
-      </Text>
+      <Text>登録した料理は、一覧画面で確認することができます。</Text>
+      <Text>一覧画面の料理名をクリックすると、詳細画面に遷移します。</Text>
+      <Text>詳細画面では、料理の詳細を確認することができます。</Text>
 
       <Image src="/images/example_image2.png" alt="説明画像2" />
 
       <Heading as="h2" size="md" mt={6} mb={4}>
         こんだてずかんの特徴
       </Heading>
-      <Text>
-        料理のカテゴリを登録することができます。
-      </Text>
+      <Text>料理のカテゴリを登録することができます。</Text>
+
+      <Text>料理の参考URLを登録することができます。</Text>
 
       <Text>
-        料理の参考URLを登録することができます。
+        料理の一覧を表示する際に、カテゴリ・ジャンルごとに絞り込むことができます。
       </Text>
 
-      <Text>
-        料理の一覧を表示する際に、カテゴリごとに絞り込むことができます。
-      </Text>
-
-      <Text>
-        材料名から料理を検索することができます。  
-      </Text>
-
+      <Text>材料名から料理を検索することができます。</Text>
 
       <Image src="/images/example_image3.png" alt="説明画像3" />
-
     </Box>
   );
 });
